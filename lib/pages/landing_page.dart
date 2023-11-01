24664b7fc22d91d31bf0b51e32f1382aad17d5f5
shakeelahammedabdulla <4pa20cs103@pace.edu.in>
2023-11-01 21:26:45 +0530

    text replacement

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,7 +1,82 @@
+// import 'package:flutter/material.dart';
+
+// class LandingPage extends StatefulWidget {
+//   const LandingPage({super.key});
+
+//   @override
+//   State<LandingPage> createState() => _LandingPageState();
+// }
+
+// class _LandingPageState extends State<LandingPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+//         child: Column(
+//           children: [
+//             ClipRRect(
+//               borderRadius: BorderRadius.circular(20),
+//               child: Image.asset(
+//                 "images/building.jpg",
+//                 width: MediaQuery.of(context).size.width -
+//                     20, // Corrected the width calculation
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20.0,
+//             ),
+//             Text(
+//               "News from around the\n          world for you",
+//               style: TextStyle(
+//                 color: Colors.black,
+//                 fontSize: 20.0,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20.0,
+//             ),
+//             Text(
+//               "Best time to read, take your time to read\n           little more of this world",
+//               style: TextStyle(
+//                 color: Colors.black,
+//                 fontSize: 20.0,
+//                 fontWeight: FontWeight.w500,
+//               ),
+//             ),
+//             SizedBox(height: 40.0,),
+
+//             Container(
+//             width: MediaQuery.of(context).size.width/1.2,
+//             padding: EdgeInsets.symmetric(vertical: 15.0),
+//                 decoration: BoxDecoration(
+//                   color: Colors.blue,
+//                   borderRadius: BorderRadius.circular(30),
+//                 ),
+//                 child: Center(
+//                   child: Text(
+//                     "Get Started",
+//                     style: TextStyle(
+//                         color: Colors.amber,
+//                         fontSize: 16.0,
+//                         fontWeight: FontWeight.w500),
+//                   ),
+//                 ),
+//               ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatefulWidget {
-  const LandingPage({super.key});
+  const LandingPage({Key? key}) : super(key: key);
 
   @override
   State<LandingPage> createState() => _LandingPageState();
@@ -14,13 +89,13 @@ class _LandingPageState extends State<LandingPage> {
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: Image.asset(
                 "images/building.jpg",
-                width: MediaQuery.of(context).size.width -
-                    20, // Corrected the width calculation
+                width: MediaQuery.of(context).size.width - 20,
                 fit: BoxFit.cover,
               ),
             ),
@@ -28,7 +103,7 @@ class _LandingPageState extends State<LandingPage> {
               height: 20.0,
             ),
             Text(
-              "News from around the\n          world for you",
+              "News from around the\n        world for you",
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 20.0,
@@ -39,32 +114,38 @@ class _LandingPageState extends State<LandingPage> {
               height: 20.0,
             ),
             Text(
-              "Best time to read, take your time to read\n           little more of this world",
+              "Best time to read, take your time to read\n              a little more of this world",
               style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
+                color: Colors.black45,
+                fontSize: 18.0,
                 fontWeight: FontWeight.w500,
               ),
             ),
-            SizedBox(height: 40.0,),
-
-            Container(
-            width: MediaQuery.of(context).size.width/1.2,
-            padding: EdgeInsets.symmetric(vertical: 15.0),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(30),
+            SizedBox(
+              height: 20.0,  // Adjusted the SizedBox height to provide some spacing.
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Add your action here
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
                 ),
-                child: Center(
-                  child: Text(
-                    "Get Started",
-                    style: TextStyle(
-                        color: Colors.amber,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w500),
+              ),
+              child:Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
               ),
+            ),
           ],
         ),
       ),
