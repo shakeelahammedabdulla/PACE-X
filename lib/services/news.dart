cc59b909e2c5d65910cbafc2bdeaea386cf709b4
shakeelahammedabdulla <4pa20cs103@pace.edu.in>
2023-11-09 15:25:31 +0530

    Merge remote changes

--- lib/services/news.dart
+++ lib/services/news.dart
@@ -71,40 +71,79 @@
 //   }
 // }
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// import 'dart:convert';
+
+// import 'package:pacex/model/article_model.dart';
+// import 'package:http/http.dart' as http;
+
+// class News {
+//   List<ArticleModel> news = [];
+
+//   Future<void> getNews() async {
+//     String url =
+//         "https://newsapi.org/v2/everything?q=tesla&from=2023-10-04&sortBy=publishedAt&apiKey=API_KEY";
+
+//     var response = await http.get(Uri.parse(url));
+
+//     if (response.statusCode == 200) {
+//       var jsonData = jsonDecode(response.body);
+
+//       if (jsonData["status"] == 'ok') {
+//         (jsonData["articles"] as List).forEach((element) {
+//           if (element["urlToImage"] != null && element["description"] != null) {
+//             ArticleModel articleModel = ArticleModel(
+//               title: element["title"],
+//               description: element["description"],
+//               url: element["url"],
+//               urlToImage: element["urlToImage"],
+//               content: element['content'],
+//               author: element["author"],
+//             );
+
+//             news.add(articleModel);
+//           }
+//         });
+//       }
+//     }
+//   }
+// }
+
+//////////////////////////////////////////////////////////////////////////////////
+///
+///import 'dart:convert';
 
 import 'dart:convert';
 
-import 'package:pacex/model/article_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:pacex/model/article_model.dart';
 
-class News {
-  List<ArticleModel> news = [];
+class News{
+  List<ArticleModel> news=[];
+  
+  
+  Future<void> getNews()async{
+String url="https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=54145bc9681c42de9a6cc831aa90502b";
+ var response= await http.get(Uri.parse(url));
 
-  Future<void> getNews() async {
-    String url =
-        "https://newsapi.org/v2/everything?q=tesla&from=2023-10-04&sortBy=publishedAt&apiKey=API_KEY";
+var jsonData= jsonDecode(response.body);
 
-    var response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      var jsonData = jsonDecode(response.body);
-
-      if (jsonData["status"] == 'ok') {
-        (jsonData["articles"] as List).forEach((element) {
-          if (element["urlToImage"] != null && element["description"] != null) {
-            ArticleModel articleModel = ArticleModel(
-              title: element["title"],
-              description: element["description"],
-              url: element["url"],
-              urlToImage: element["urlToImage"],
-              content: element['content'],
-              author: element["author"],
-            );
-
-            news.add(articleModel);
-          }
-        });
-      }
+if(jsonData['status']=='ok'){
+  jsonData["articles"].forEach((element){
+    if(element["urlToImage"]!=null && element['description']!=null){
+      ArticleModel articleModel= ArticleModel(
+        title: element["title"],
+        description: element["description"],
+        url: element["url"],
+        urlToImage: element["urlToImage"],
+        content: element["content"],
+        author: element["author"],
+      );
+      news.add(articleModel);
     }
+  });
+}
+ 
   }
 }
